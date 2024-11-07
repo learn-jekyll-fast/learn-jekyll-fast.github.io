f0ad757cff1cb3ac3514da6812b493260504da27
Amanpreet <aman.xennial@gmail.com>
2024-11-07 12:45:01 -0500

    update okta

--- js/okta.js
+++ js/okta.js
@@ -11,11 +11,7 @@ async function login() {
 }
 
 async function retreiveData() {
-  auth0 = await createAuth0Client({
-    domain: 'dev-fzaxiig2nqftlaxz.us.auth0.com', // Your Auth0 domain
-    client_id: '25XWQm2rT7G0jxoUblSo4Ec3yZCEd8wG', // Your Auth0 client ID
-    redirect_uri: 'https://learn-jekyll-fast.github.io/dashboard.html' // Adjust the redirect URI as needed
-  });
+
   const redirectResult = await auth0.handleRedirectCallback();
   console.log('redirectResult:  ' + JSON.stringify(redirectResult));
   //logged in. you can get the user profile like this:
